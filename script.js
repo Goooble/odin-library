88a087bf18753cbee885ef734a38e61526f6bcb4
Goooble <[bivinbheemaiah@gmail.com]>
2024-08-20 12:32:23 +0530

    read button fixed, cards are displayed properly read status is preserved after every submit

--- script.js
+++ script.js
@@ -1,59 +1,98 @@
 const myLibrary = [];
 
-function Book(author, title, pages, read) {
+function Book(title, author, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
 }
 
-function addBook(author, title, pages, read) {
+function addBook(title, author, pages, read) {
   myLibrary.unshift(new Book(title, author, pages, read));
 }
-
-addBook("Dune", "Frank Hubert", 600, true);
-addBook("Dune2", "Frank Hubert2", 6002, true);
-addBook("Dune3", "Frank Hubert3", 6003);
-console.log(myLibrary);
-
 
 const dialog = document.querySelector("dialog");
 const addBookBut = document.querySelector(".add");
 const main = document.querySelector("main");
+const removeBut = document.querySelector(".remove");
+var cardList = null;
 
-
-//cards
 printCards();
-function printCards(){
-    myLibrary.forEach((book, index)=>{
-        var cardHTML = `<div>
+//card display functions
+function printCards() {
+  myLibrary.forEach((book, index) => {
+    var readingStatus;
+    book.read ? (readingStatus = "read-true") : (readingStatus = "read-false");
+    var cardHTML = `<div class="card${index}" >
             <div class="details">
                 <p class="title">${book.title}</p>
                 <p class="author">by ${book.author}</p>
                 <p class="pages">${book.pages} pages</p>
             </div>
             <div class="buttons">
-                <button class="read read-status"> Read</button>
-                <button class="remove">Remove</button>
+                <button class="read ${readingStatus}"> Read</button>
+                <button class="card${index} remove">Remove</button>
             </div> 
         </div>`;
-        main.insertAdjacentHTML("beforeend", cardHTML);
-    })
+    main.insertAdjacentHTML("beforeend", cardHTML);
+  });
+  //to update the cards in dom
+  cardList = document.querySelectorAll("main>div");
+
 }
+function updateCards() {
+  cardList.forEach((e) => {
+    main.removeChild(e);
+  });
+  printCards();
+}
+
+main.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove")) {
+    var index = getIndex(e.target);
+    myLibrary.splice(index, 1);
+    updateCards();
+  }
+});
 
 //read status toggling
-const readBut = Array.from(document.querySelectorAll(".read"));
 main.addEventListener("click", (e) => {
-    if(e.target.classList.contains("read")){
-        toggleReadStat(e.target);
+  if (e.target.classList.contains("read")) {
+    toggleReadStat(e.target);
+    if (e.target.classList.contains("read-false")) {
+      myLibrary[getIndex(e.target.nextElementSibling)].read = false;
+    } else {
+      myLibrary[getIndex(e.target.nextElementSibling)].read = true;
     }
-})
+    
+  }
+  console.log(myLibrary)
+});
 
-function toggleReadStat(button){
-    button.classList.toggle("read-status");
+function getIndex(removeBut) {
+  return removeBut.classList.item(0).slice(4);
 }
 
-
+function toggleReadStat(button) {
+  button.classList.toggle("read-false");
+}
 
 //dialog
+const inTitle = document.querySelector("#title");
+const inAuthor = document.querySelector("#author");
+const inPages = document.querySelector("#pages");
+const inRead = document.querySelector("#read-box");
+dialog.returnValue = null;
+
 addBookBut.addEventListener("click", () => dialog.showModal());
+
+dialog.addEventListener("close", () => {
+  if (dialog.returnValue === "submit") {
+    addBook(inTitle.value, inAuthor.value, inPages.value, inRead.checked);
+    updateCards();
+  }
+  inTitle.value = "";
+  inAuthor.value = "";
+  inPages.value = "";
+  inRead.checked = true;
+});
